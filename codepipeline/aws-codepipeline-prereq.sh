4f391ec3a2d36a87b8c1180ba39b568332bea2f8
Carlos Zambrano <czam01@gmail.com>
2019-08-18 22:26:35 -0300

    non nested stacks

--- codepipeline/aws-codepipeline-prereq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-aws cloudformation deploy \
---region us-east-2 \
---stack-name codepipeline-platzi-prereq \
---template-file ./aws-codepipeline-prereq.yml \
---capabilities CAPABILITY_IAM \
---capabilities CAPABILITY_NAMED_IAM
